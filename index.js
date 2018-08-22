0bc7768e1a077c308d9172f34db94c6096dfed64
Mark Ehler <ehlerm42@gmail.com>
2018-08-22 02:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+var getFristSelector(selector)
+
+
+
+var nestedTarget()
+
+#IDselectors
+
+var increaseRankBy(n)
+//increases .ranked-list by n
+
+
+var deepestChild()
+
+//most deeply nested child from div#grand-node
+
